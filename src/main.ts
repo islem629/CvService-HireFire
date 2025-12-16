a3011802cde38356df31be3a76d6fd1295e5e019
islem629 <islemhamdi368@gmail.com>
2025-12-16 22:51:17 +0100

    Update cv service (deps, tracing, main)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { eurekaClient } from './eureka';
+import './tracing';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
